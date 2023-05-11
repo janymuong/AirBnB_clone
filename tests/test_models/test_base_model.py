4af86eac3ef2e69ba38a5f7ff3d71dc1893ef8b4
Mosesbwire <mosesdev92@gmail.com>
2023-05-11 10:49:28 +0000

    Add tests for save and to_dict functions

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -153,7 +153,7 @@ class TestBaseModel(unittest.TestCase):
                 Expected:\n\t{}\nActual:\n\t{}".format(datetime.now(),
                                                        model.updated_at)
 
-        self.assertAlmostEqual(model.updated_at, datetime.now,
+        self.assertAlmostEqual(model.updated_at, datetime.now(),
                                delta=timedelta(milliseconds=10), msg)
 
     def test_created_at_not_updated_on_updates(self):
@@ -193,29 +193,6 @@ class TestBaseModel(unittest.TestCase):
         self.assertIsInstance(model.created_at, datetime)
         self.assertIsInstance(model.updated_at, datetime)
 
-    def test_created_at_and_updated_at_is_str(self):
-        """
-            test that created_at and updated_at are str objects
-        """
-        model = BaseModel()
-        self.assertIsInstance(model.created_at, str)
-        self.assertTrue(type(model.created_at) == str)
-        self.assertIsInstance(model.updated_at, str)
-        self.assertTrue(type(model.updated_at) == str)
-
-    def test_created_at_and_updated_at_are_from_datetime(self):
-        """
-            test that created_at and updated_at were created from datetime\
-                    objects
-        """
-        model = BaseModel()
-        isoFormat = "%Y-%m-%dT%H:%M:%S.%f"
-        created_datetime = datetime.strptime(model.created_at, isoFormat)
-        updated_datetime = datetime.strptime(model.updated_at, isoFormat)
-
-        self.assertIsInstance(created_datetime, datetime)
-        self.assertIsInstance(updated_datetime, datetime)
-
     def test_str_method_is_present(self):
         """
             tests that the __str__ function is available
@@ -248,3 +225,112 @@ class TestBaseModel(unittest.TestCase):
         msg = "Expected {} but got {}\nExpected:\n\t{}\nActual:\n\t{}".format(
             expected, actual, expected, actual)
         self.assertEqual(expected, actual, msg)
+
+    def test_save_function_is_present(self):
+        """
+            tests that BaseModel has function save()
+        """
+        model = BaseModel()
+        msg = "Expected {} to have a 'save()' function".format(
+            type(model).__name__)
+
+        self.assertTrue(hasAttr(model, 'save'), msg)
+        self.assertIn('save', model.__dict__.keys(), msg)
+
+    def test_save_function_updates_updated_at_with_current_time(self):
+        """
+            tests that the on calling save(), the public attribute created_at\
+                    is updated with the current time
+        """
+        model = BaseModel()
+        model.save()
+        expected = datetime.now()
+        actual = model.updated_at
+        msg = "Expected updated_at to be updated with the current time {} but\
+                but got {}".format(expected, actual)
+
+        self.assertAlmostEqual(expected, actual,
+                               delta=timedelta(10=milliseconds), msg)
+
+    def test_to_dict_is_present(self):
+        """
+            tests that the to_dict function is defined
+        """
+        model = BaseModel()
+        msg = "Expected {} to have a 'to_dict' function".format(
+            type(model).__name__)
+
+        self.assertTrue(hasAttr(model, 'to_dict'), msg)
+
+    def test_to_dict_return_value_is_dictionary(self):
+        """
+            tests the return value of to_dict is a dictionary
+        """
+        model = BaseModel()
+        actual = model.to_dict()
+        msg = "Expected return value to be a dictionary but got {}".format(
+            type(actual))
+
+        self.assertIsInstance(actual, dict)
+
+    def test_to_dict_return_value_not_empty(self):
+        """
+            tests that the return value of to_dict is not an empty dict
+        """
+        model = BaseModel()
+        actual = model.to_dict()
+        msg = "Expected return value to not be an empty dict but got\
+                {}".format(actual)
+        self.assertTrue(bool(actual), msg)
+
+    def test_to_dict_return_value_has_attr_class(self):
+        """
+            tests that the return value(dict) has key __class__
+            tests that value of __class__ is name of object's class
+        """
+        model = BaseModel()
+        actual = model.to_dict()
+        msg = "Expected the returned dict to have key '__class__'"
+
+        self.assertIn("__class__", actual, msg)
+        self.assertEqual(actual.__class__, type(model).__name__, "Expected\
+                value of __class__ to be 'BaseModel' but got {}".format(
+            actual.__class__))
+
+    def test_to_dict_return_value_has_values_of_dict(self):
+        """
+            tests that returned dict from to_dict has all the key/value\
+                    pairs of __dict__
+        """
+        model = BaseModel()
+        actual = model.to_dict()
+        msg = "Expected all key/values here: {} to be in {}".format(
+            model.__dict__, actual)
+
+        self.assertDictContainSubset(model.__dict__, actual, msg)
+
+    def test_created_at_and_updated_saved_as_str(self):
+        """
+            test that created_at and updated_at are str objects when\
+                    serialized into dict
+        """
+        model = BaseModel()
+        dict_value = model.to_dict()
+        self.assertIsInstance(dict_value.created_at, str)
+        self.assertTrue(type(dict_value.created_at) == str)
+        self.assertIsInstance(dict_value.updated_at, str)
+        self.assertTrue(type(dict_value.updated_at) == str)
+
+    def test_created_at_and_updated_at_are_from_datetime(self):
+        """
+            test that created_at and updated_at were created from datetime\
+                    objects and serialized in correct format
+        """
+        model = BaseModel()
+        dict_value = model.to_dict()
+        isoFormat = "%Y-%m-%dT%H:%M:%S.%f"
+        created_datetime = datetime.strptime(dict_value.created_at, isoFormat)
+        updated_datetime = datetime.strptime(dict_value.updated_at, isoFormat)
+
+        self.assertIsInstance(created_datetime, datetime)
+        self.assertIsInstance(updated_datetime, datetime)
