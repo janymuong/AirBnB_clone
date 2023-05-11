011ba5bdf9d4c6419d3559983760717fc9354aef
Mosesbwire <mosesdev92@gmail.com>
2023-05-11 08:56:43 +0000

    Add test for the __str__ function. Add test messages to the created_at and updated_at test cases

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -107,17 +107,26 @@ class TestBaseModel(unittest.TestCase):
                     when object is created
         """
         model = BaseModel()
+
         self.assertAlmostEqual(model.created_at, datetime.now(),
-                               delta=timedelta(milliseconds=10))
+                               delta=timedelta(milliseconds=10),
+                               "Expected created_at to be set to time now on\
+                                       creation.")
         self.assertAlmostEqual(model.updated_at, datetime.now(),
-                               delta=timedelta(milliseconds=10))
+                               delta=timedelta(milliseconds=10),
+                               "Expected updated_at to be set to time now on\
+                                       creattion.")
 
     def test_created_at_and_updated_at_are_equal_on_create(self):
         """
             tests that created_at == updated_at on create
         """
         model = BaseModel()
-        self.assertEqual(model.created_at, model.updated_at)
+
+        msg = "Expected created_at: {} to be equal to updated_at: {}".format(
+            model.created_at, model.updated_at)
+
+        self.assertEqual(model.created_at, model.updated_at, msg)
 
     def test_updated_at_is_updated_on_updates(self):
         """
@@ -128,7 +137,11 @@ class TestBaseModel(unittest.TestCase):
         updated_at = model.updated_at
 
         model.update = "this is an update"
-        self.assertNotEqual(updated_at, model.updated_at)
+
+        msg = "Expected updated_at:[before update] {} not to be equal to\
+                updated_at: [after update] {}".format(
+            updated_at, model.updated_at)
+        self.assertNotEqual(updated_at, model.updated_at, msg)
 
     def test_updated_at_is_set_to_current_time_on_update(self):
         """
@@ -136,17 +149,27 @@ class TestBaseModel(unittest.TestCase):
         """
         model = BaseModel()
         model.update = "this is an update"
+        msg = "Expected updated_at to be set to current time on update\n.\
+                Expected:\n\t{}\nActual:\n\t{}".format(datetime.now(),
+                                                       model.updated_at)
+
         self.assertAlmostEqual(model.updated_at, datetime.now,
-                               delta=timedelta(milliseconds=10))
+                               delta=timedelta(milliseconds=10), msg)
 
     def test_created_at_not_updated_on_updates(self):
         """
             test that created_at is not updated when an update is done
         """
         model = BaseModel()
+
         created_at = model.created_at
+
         model.update = "this is an update"
-        self.assertEqual(created_at, model.created_at)
+
+        msg = "Expected created_at to not be updated on updates.\nExpected:\
+                \n\t{}\nActual:\n\t{}".format(created_at, model.created_at)
+
+        self.assertEqual(created_at, model.created_at, msg)
 
     def test_updated_at_greater_than_created_at_on_updates(self):
         """
@@ -157,7 +180,18 @@ class TestBaseModel(unittest.TestCase):
 
         model.update = "is an update"
 
-        self.assertGreater(model.updated_at, created_at)
+        msg = "Expected updated_at: {} to be > than created_at: {} after\
+                update".format(model.updated_at, model.created_at)
+
+        self.assertGreater(model.updated_at, created_at, msg)
+
+    def test_updated_at_and_created_at_is_datetime(self):
+        """
+            test that updated_at and created_at are datetime objects
+        """
+        model = BaseModel()
+        self.assertIsInstance(model.created_at, datetime)
+        self.assertIsInstance(model.updated_at, datetime)
 
     def test_created_at_and_updated_at_is_str(self):
         """
@@ -181,3 +215,36 @@ class TestBaseModel(unittest.TestCase):
 
         self.assertIsInstance(created_datetime, datetime)
         self.assertIsInstance(updated_datetime, datetime)
+
+    def test_str_method_is_present(self):
+        """
+            tests that the __str__ function is available
+        """
+        model = BaseModel()
+        msg = "Expected {} to have a __str__ function".format(
+            type(model).__name__)
+        self.assertTrue(hasAttr(model, '__str__'), msg)
+        self.assertIn('__str__', model.__dict__.keys(), msg)
+
+    def test_str_method_returns_string(self):
+        """
+            test that return value of __str__ is a string
+        """
+        model = BaseModel()
+        value = str(model)
+        msg = "Expected return value of __str__ to string but got {}".format(
+            type(value))
+        self.assertIsInstance(value, str, msg)
+        self.assertTrue(type(value) == str, msg)
+
+    def test_str_return_is_correct_format(self):
+        """
+            tests that the return value of __str__ is in stipulated format
+        """
+        model = BaseModel()
+        expected = "[{}] ({}) {}".format(
+            type(model).__name__, model.id, model.__dict__)
+        actual = str(model)
+        msg = "Expected {} but got {}\nExpected:\n\t{}\nActual:\n\t{}".format(
+            expected, actual, expected, actual)
+        self.assertEqual(expected, actual, msg)
